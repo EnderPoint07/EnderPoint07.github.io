ccda3b6fc868882c4a4e3f8500e98691129b1e20
EnderPoint07 <anshhd64@hotmail.com>
2023-05-27 19:08:40 +0530

    smoll change

--- sidenav.js
+++ sidenav.js
@@ -4,15 +4,15 @@ var arrow = document.getElementById("arrow")
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+    dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-      arrow.classList = "arrow right"
+        dropdownContent.style.display = "none";
+        arrow.classList = "arrow right"
     } else {
-      dropdownContent.style.display = "block";
-      arrow.classList = "arrow down"
+        dropdownContent.style.display = "block";
+         arrow.classList = "arrow down"
     }
   });
 }
