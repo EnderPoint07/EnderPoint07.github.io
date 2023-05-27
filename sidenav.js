f3d8326c5d6be54a7e8d8e91fad9cc9837d29869
EnderPoint07 <anshhd64@hotmail.com>
2023-05-27 19:01:19 +0530

    added the arrow thingy to show it a dropdown

--- sidenav.js
+++ sidenav.js
@@ -1,5 +1,6 @@
 // Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict
 var dropdown = document.getElementsByClassName("dropdown-btn");
+var arrow = document.getElementById("arrow")
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
@@ -8,8 +9,10 @@ for (i = 0; i < dropdown.length; i++) {
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
+      arrow.classList = "arrow right"
     } else {
       dropdownContent.style.display = "block";
+      arrow.classList = "arrow down"
     }
   });
 }
