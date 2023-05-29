05448d81555cee42fe04d2750026e20eb6b45847
EnderPoint07 <anshhd64@hotmail.com>
2023-05-29 16:55:40 +0530

    added support for multiple dropdowns' arrow

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,18 +1,18 @@
 // Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var arrow = document.getElementById("arrow")
+var dropdowns = document.getElementsByClassName("dropdown-btn");
 var i;
 
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
+for (i = 0; i < dropdowns.length; i++) {
+  dropdowns[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
+    var arrow = this.firstElementChild
     if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-        arrow.classList = "arrow right"
+      dropdownContent.style.display = "none";
+      arrow.classList = "arrow right"    
     } else {
-        dropdownContent.style.display = "block";
-         arrow.classList = "arrow down"
+      dropdownContent.style.display = "block";
+      arrow.classList = "arrow down"
     }
   });
 }
