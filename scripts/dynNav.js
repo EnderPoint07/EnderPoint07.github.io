57adf42464440c8aa4af8dcedcbaa58f15492740
EnderPoint07 <anshhd64@hotmail.com>
2023-06-18 17:58:26 +0530

    added some resources to resources.html + minor changes

--- scripts/dynNav.js
+++ scripts/dynNav.js
@@ -4,7 +4,7 @@ function scaleTopNav() {
     setInterval(() => {
         var sideNavWidth = document.body.scrollWidth * 12/100;
         Array.from(topnavLinks).forEach(link => {
-            link.style.width = ((document.body.scrollWidth - sideNavWidth) * 20/100 - 47) + "px";
+            link.style.width = ((document.body.scrollWidth - sideNavWidth) * 20/100 - 50) + "px";
         });
     }, 100)
 }
